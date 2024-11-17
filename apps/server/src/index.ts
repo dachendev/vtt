af3d5e87db9e6e907e2eec446cbec706757cf2c4
dachendev <13964975+dachendev@users.noreply.github.com>
2024-11-17 15:57:22 -0500

    Add cors middleware

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import morgan from "morgan";
 import { errorHandler } from "./middleware/errorHandler";
 import { mountRoutes } from "./mountRoutes";
@@ -7,6 +8,7 @@ const app = express();
 const port = 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 
@@ -21,4 +23,3 @@ app.use(errorHandler);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
